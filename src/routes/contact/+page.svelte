4fcddcf21151b2a6e5399471a3068dddde2bd52a
Meek Denzo <meekdenzo@gmail.com>
2024-05-24 00:54:29 -0500

    chore(ngrok error): fixed / validation for  route but still won't work outside network on NAME:   ngrok - tunnel local ports to public URLs and inspect traffic
    USAGE:
      ngrok [command] [flags]
    
    DESCRIPTION:
      ngrok exposes local networked services behinds NATs and firewalls to the
      public internet over a secure tunnel. Share local websites, build/test
      webhook consumers and self-host personal services.
      Detailed help for each command is available with 'ngrok help <command>'.
      Open http://localhost:4040 for ngrok's web interface to inspect traffic.
    
    Author:
      ngrok - <support@ngrok.com>
    
    TERMS OF SERVICE: https://ngrok.com/tos
    
    EXAMPLES:
      ngrok http 80                           # secure public URL for port 80 web server
      ngrok http --domain baz.ngrok.dev 8080  # port 8080 available at baz.ngrok.dev
      ngrok http foo.dev:80                   # tunnel to host:port instead of localhost
      ngrok http https://localhost            # expose a local https server
      ngrok tcp 22                            # tunnel arbitrary TCP traffic to port 22
      ngrok tls --domain=foo.com 443          # TLS traffic for foo.com to port 443
      ngrok start foo bar baz                 # start tunnels from the configuration file
    
    COMMANDS:
      api                            use ngrok agent as an api client
      completion                     generates shell completion code for bash or zsh
      config                         update or migrate ngrok's configuration file
      credits                        prints author and licensing information
      diagnose                       diagnose connection issues
      help                           Help about any command
      http                           start an HTTP tunnel
      service                        run and control an ngrok service on a target operating system
      start                          start tunnels by name from the configuration file
      tcp                            start a TCP tunnel
      tls                            start a TLS tunnel
      tunnel                         start a tunnel for use with a tunnel-group backend
      update                         update ngrok to the latest version
      version                        print the version string
    
    OPTIONS:
          --config strings   path to config files; they are merged if multiple
      -h, --help             help for ngrok
      -v, --version          version for ngrok

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,7 +8,9 @@
 
     const schema = z.object({
         name: z.string().min(2),
-        email: z.string().email()
+        email: z.string().email(),
+        subject: z.string().min(2),
+        message: z.string().min(2),
     });
 
 
@@ -21,48 +23,162 @@
     );
 </script>
 
-<div>
-    {#each Array(10) as _, i}
-        <br>    {/each}
-</div>
-<SuperDebug data={$form}/>
-<br>
-<article>
+<form method="POST" use:enhance>
+    <div class="input-group">
+        <input type="text" class="input-field" placeholder=" " name="name" bind:value={$form.name}/>
+        <label for="name" class="input-label">Your Name</label>
+    {#if $errors.name}<small class="invalid">{$errors.name}</small>{/if}
+    </div>
 
-    {#if $message}<h3>{$message}</h3>{/if}
+    <div class="input-group">
+        <input type="text" class="input-field" placeholder=" " name="email" bind:value={$form.email}/>
+        <label for="email" class="input-label">Your Email</label>
+    {#if $errors.email}<small class="invalid">{$errors.email}</small>{/if}
+    </div>
 
-    <form method="POST" use:enhance>
-        <label for="name">Name</label>
-        <input
-                type="text"
-                name="name"
-                id="name"
-                bind:value={$form.name}
-        />
-        {#if $errors.name}<small class="invalid">{$errors.name}</small>{/if}
+    <div class="input-group">
+        <input type="text" class="input-field" placeholder=" " name="subject" bind:value={$form.subject}/>
+        <label for="subject" class="input-label">Subject</label>
+    {#if $errors.subject}<small class="invalid">{$errors.subject}</small>{/if}
+    </div>
 
-        <label for="email">E-mail</label>
-        <input
-                type="email"
-                name="email"
-                id="email"
-                bind:value={$form.email}
-        />
-        {#if $errors.email}<small class="invalid">{$errors.email}</small>{/if}
+    <textarea
+            id="message"
+            placeholder="Type your message here..."
+            name="message"
+            rows="4"
+            cols="50"
+            bind:value={$form.message}
+    ></textarea>
+    {#if $errors.message}<small class="invalid">{$errors.message}</small>{/if}
+    <input class="submit-button" type="submit" value="Send"/>
 
-        <div>
-            <button>Submit</button>
-        </div>
-    </form>
-</article>
+    <input type="hidden" name="replyTo" value="@"/>
+</form>
 
 <style>
+    form {
+        /*display: inline;*/
+        /* position: fixed; */
+        /*   left: 100%;*/
+        transform: translateX(50%);
+        max-width: 700px;
+        display: grid;
+        width: 100%;
+        /*gap: 0.5rem;*/
+        gap: var(--spacing-sm);
+        margin-top: var(--spacing-xxl);
+        /*padding: 5rem 1rem 2rem;*/
+        /*border: var(--debug);*/
+    }
+
+    #message {
+        font-size: var(--normal-font-size);
+        border: 1px solid var(--sprobest-light-secondary-text);
+        border-radius: 0.5rem;
+        outline: none;
+        padding: 1rem;
+        background: none;
+        min-height: 20rem;
+        /* background: var(--sprobest-dark-bg); */
+    }
+
+    .input-group {
+        position: relative;
+        height: 48px;
+        margin-bottom: 1.5rem;
+    }
+
     .invalid {
+        position: absolute;
+        bottom: -1.25rem; /* Adjust the value as needed */
+        left: 0;
+        color: red; /* Change the color as desired */
+        font-size: 0.8rem; /* Adjust the font size as needed */
+    }
+
+    .input-field {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        font-size: var(--normal-font-size);
+        border: 1px solid var(--sprobest-light-secondary-text);
+        border-radius: 0.5rem;
+        outline: none;
+        padding: 1rem;
+        background: none;
+        z-index: 1;
+    }
+
+    .input-label {
+        position: absolute;
+        left: var(--spacing-md);
+        top: 50%;
+        transform: translateY(-50%);
+        color: var(--input-color);
+        font-size: var(--normal-font-size);
+        transition: 0.3s;
+        z-index: 9999;
+        /*border: var(--debug);*/
+    }
+
+    .submit-button {
+        display: block;
+        /*margin: 0 auto; !*TBD*!*/
+        max-width: 20%;
+        padding: 0.75rem 2rem;
+        outline: none;
+        border: none;
+        background-color: var(--sprobest-light-secondary-bg);
+        color: var(--sprobest-light-text);
+        font-size: var(--normal-font-size);
+        border-radius: 0.5rem;
+        cursor: pointer;
+        transition: 0.3s;
+    }
+
+    .submit-button:hover {
+        /*box-shadow: 0 5px 16px black;*/
+        /*box-shadow: 0 var(--spacing-xs) var(--spacing-xs) var(--sprobest-light-shadow);*/
+        border: 1.5px solid var(--sprobest-light-text);
+    }
+
+    /* Move label up on input focus */
+
+    .input-field:focus + .input-label,
+    .input-field:not(:placeholder-shown).input-field:not(:focus) + .input-label {
+        top: 0;
+        left: var(--spacing-md);
+        padding: 0 var(--spacing-sm);
+        color: var(--sprobest-light-text);
+        font-size: var(--small-font-size);
+        font-weight: 500;
+        z-index: 10;
+        background-color: var(--sprobest-light-bg);
+        /*border: var(--debug);*/
+    }
+
+    /* Border color change on input focus */
+    .input-field:focus {
+        border: 1.5px solid var(--sprobest-light-text);
+    }
+
+    #message:focus {
+        border: 1.5px solid var(--sprobest-light-text);
+    }
+
+    .success {
+        color: green;
+    }
+
+    .error {
         color: red;
     }
 
-    input {
-        border: 1px solid red;
-        background: white;
-    }
+    /*.invalid {*/
+    /*    bottom: 0;*/
+    /*    color: blue;*/
+    /*}*/
 </style>
