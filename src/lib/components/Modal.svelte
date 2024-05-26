05cf9a705fb0d1dc527e6ff5d120e7d96f0ed58e
Meek Denzo <meekdenzo@gmail.com>
2024-05-26 14:16:21 -0500

    mod: refactor `Modal.svelte` using daisy components

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,29 +1,39 @@
-<script>
-  import { Button, Modal } from "flowbite-svelte";
-  let clickOutsideModal = false;
-</script>
+<!-- Open the modal using ID.showModal() method -->
+<button class="btn" onclick="my_modal_1.showModal()">open modal</button>
+<dialog id="my_modal_1" class="modal">
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">Hello!</h3>
+    <p class="py-4">Press ESC key or click the button below to close</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
 
-<Button on:click={() => (clickOutsideModal = true)}>Default modal</Button>
+<style>
+  dialog {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    background: var(--sprobest-dark-bg-see-through);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1000;
+  }
 
-<Modal
-  title="Terms of Service"
-  bind:open={clickOutsideModal}
-  autoclose
-  outsideclose
->
-  <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-    With less than a month to go before the European Union enacts new consumer
-    privacy laws for its citizens, companies around the world are updating their
-    terms of service agreements to comply.
-  </p>
-  <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into
-    effect on May 25 and is meant to ensure a common set of data rights in the
-    European Union. It requires organizations to notify users as soon as
-    possible of high-risk data breaches that could personally affect them.
-  </p>
-  <svelte:fragment slot="footer">
-    <Button on:click={() => alert('Handle "success"')}>I accept</Button>
-    <Button color="alternative">Decline</Button>
-  </svelte:fragment>
-</Modal>
+  .modal-box {
+    background: var(--sprobest-light-bg-see-through);
+    border-radius: 1rem;
+    padding: 1rem;
+    width: 300px;
+    max-width: 90%;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  }
+</style>
