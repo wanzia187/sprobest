5e785a72482ce40e78c16203431e05f5b0e2aa83
wanzia187 <wanzianambule@gmail.com>
2024-08-21 00:24:11 +0200

    cleared error

--- src/lib/components/QuickBookModal.svelte
+++ src/lib/components/QuickBookModal.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     import BookingForm from "$lib/components/BookingForm.svelte";
     import {onMount} from 'svelte';
-    import {SuperForm} from "sveltekit-superforms";
-    import {z} from "zod";
 
     export let selectedEvent: string;
 
